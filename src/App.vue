1cbd65c312920c0f1b9ed3b3ce245af94b645d9e
Mason Akershoek <masonakershoek@gmail.com>
2025-11-30 13:07:27 -0500

    fixed App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <script setup>
-import PixiCanvas from './components/Game.vue'
-import Game from './components/Game.vue'
-</script>
+  import PixiCanvas from './components/Game.vue'
+  import { ref, computed } from 'vue'
+  import { Application, Graphics } from 'pixi.js'
+  import { Character } from './character';
 
-<script>
-import { ref, computed } from 'vue'
-
-// reactive properties
-const name = ref("Mason");
-const count = ref(0);
-
-// computed properties
-const doubleCount = computed(() => count.value * 2);
-
-  // methods
-  function increment(){
-    count.value++;
-  }
+  // reactive properties
+  const name = ref("Mason");
+  const count = ref(0);
+  const pixiRef = ref();
+  const character = ref();
+  const playerName = ref();
+  const characterName = ref();
 
   function makeSquare(){// Example: spinning square
     character.value = new Character("Maosn", pixiRef)
@@ -63,70 +57,5 @@ const doubleCount = computed(() => count.value * 2);
 </template>
 
 <style scoped>
-.app {
-  height: fit-content;
-  display: flex;
-  flex-direction: column;
-  background: burlywood;
-  padding: 20px;
-  border:#374151;
-  
-}
-
-.statusBar {
-  display: flex;
-  padding: 10px;
-  gap: 10px;
-}
-
-.statusLabel {
-text-transform: uppercase;
-}
-
-.statusItem {
-  flex: 1;
-  max-width: 220px;
-  gap: 0.25rem;
-}
-
-.statusBarTrack {
-  width: 100%;
-  height: 10px;
-  border-radius: 999px;
-  background: #374151;
-}
-
-.statusBarFill {
-  height: 100%;
-  width: 60%;
-  border-radius: 999px;
-}
-
-.buttonBar {
-  display: flex;
-  padding: 10px;
-  gap: 10px;
-  justify-content: space-between;
-}
-
-.actionButton {
-  padding: 10px;
-  width: 150px;
-  text-transform: uppercase;
-  text-align: center;
-
-}
-
-.statusBarFillHunger {
-  background-color: orange;
-}
-
-.statusBarFillHygiene {
-  background-color: blue;
-}
-
-.statusBarFillHappiness {
-  background-color: green;
-}
 
 </style>
